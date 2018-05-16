c5a0b9e177095ca7179ca6f706e7b0267ec8611f
Rodolfo Palma Otero <rpalmaotero@gmail.com>
2018-05-16 17:45:33 -0400

    Revert "Create Visu"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,4 @@
 //
 //= require rails-ujs
 //= require turbolinks
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-//= require gentelella-custom
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home' => 'pages#home'
-  get 'about' => 'pages#about'
-  get 'new_project' => 'pages#new_project'
-  get 'project' => 'pages#project'
-  get 'login' => 'pages#login'
-  get 'logout' => 'pages#logout'
 end
