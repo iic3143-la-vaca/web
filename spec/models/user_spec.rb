9e6bd6b9c8c9e27d172b20709ef0a8856e036a5d
ctalvarez <ctalvarez@uc.cl>
2018-05-21 19:49:58 -0400

    chore: save commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Enum test
+  it { should define_enum_for(:role) }
+
+  # Association test
+  it { should have_many(:control_users) }
+  it { should have_many(:bulges) }
+  it { should have_many(:scraps) }
+
+  # Validation tests
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:rut) }
+  it { should validate_presence_of(:dv) }
+  it { should validate_presence_of(:role) }
+  it { should validate_uniqueness_of(:rut) }
 end
