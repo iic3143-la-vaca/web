cb490eefed65ef06ce8d6089d81674d43f8b90b1
ctalvarez <ctalvarez@uc.cl>
2018-06-24 14:10:28 -0400

    fix: rubocop test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,7 @@
 class UserMailer < ApplicationMailer
-
   def rejection_email(project, reasons)
     @project = project
     @reasons = reasons
     mail(to: @project.user.email, subject: "#{@project.title} postulation has been rejected")
   end
-
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'la.vaca.iic3143@gmail.com'}
+  config.action_mailer.default_options = { from: 'la.vaca.iic3143@gmail.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
