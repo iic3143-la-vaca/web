952bfcb3b3c5db7f222bebf1cf2b9a0fd6ed8cdb
ctalvarez <ctalvarez@uc.cl>
2018-06-24 20:13:04 -0400

    feat: added commets show

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,29 +4,17 @@ class CommentsController < ApplicationController
   # before_action :set_user, only: [:create]
   before_action :authenticate_user!, only: [:create]
 
-  # api :POST, '/comment'
+  api :POST, '/comment'
   def create
-    p comment_params
-    p comment_params[:comment]
-    p @project
-
     @comment = Comment.create!(body: comment_params[:comment][:body],
                                  user_id: current_user.id,
                                  project_id: @project.id)
-    # p comment_params[:comment]
-    # p comment_params[:user_id]
-    # p comment_params[:project_id]
-    # p comment_params[:body]
-    # p 'adfdaf'
-    # @comment = Comment.create!(comment_params)
     redirect_to project_path(id: @project.id)
   end
 
   private
 
     def comment_params
-      p 'asdfnoasdnfo'
-      p params
       params.permit(:id, comment: [:body])
     end
 
